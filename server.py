ebe10dbc68dab8d39a15cd03aab87a71be567b7f
shyam-agarwal-1 <shyam.agarwal@aowealth.in>
2024-05-12 12:17:31 +0530

    added cors on options

--- server.py
+++ server.py
@@ -17,7 +17,24 @@ class CustomCORSMiddleware(BaseHTTPMiddleware):
         
         return response
 
+
+
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000"
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register the custom CORS middleware
 app.add_middleware(CustomCORSMiddleware)
