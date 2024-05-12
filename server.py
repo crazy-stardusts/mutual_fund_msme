5d4556264d7fa8761b6b2e47b82ba30c5c82f321
shyam-agarwal-1 <shyam.agarwal@aowealth.in>
2024-05-12 14:39:51 +0530

    vercel cors

--- server.py
+++ server.py
@@ -4,25 +4,13 @@ from routes.auth import router as auth_router
 from routes.protected import router as protected_router
 from starlette.middleware.base import BaseHTTPMiddleware
 
-# class CustomCORSMiddleware(BaseHTTPMiddleware):
-#     async def dispatch(self, request: Request, call_next):
-#         # Call the next middleware
-#         response = await call_next(request)
-        
-#         # Add CORS headers to the response
-#         response.headers["Access-Control-Allow-Origin"] = "*"
-#         response.headers["Access-Control-Allow-Credentials"] = "true"
-#         response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-#         response.headers["Access-Control-Allow-Headers"] = "*"
-        
-#         return response
 
 
 
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000"
+    "https://angelhack-banglore.vercel.app"
 ]
 
 app.add_middleware(
