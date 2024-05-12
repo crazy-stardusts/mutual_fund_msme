6a210d8e631aa11aa5a759e8b9c728c0dc50d4f8
shyam-agarwal-1 <shyam.agarwal@aowealth.in>
2024-05-12 12:32:34 +0530

    added cors on options

--- server.py
+++ server.py
@@ -4,18 +4,18 @@ from routes.auth import router as auth_router
 from routes.protected import router as protected_router
 from starlette.middleware.base import BaseHTTPMiddleware
 
-class CustomCORSMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        # Call the next middleware
-        response = await call_next(request)
+# class CustomCORSMiddleware(BaseHTTPMiddleware):
+#     async def dispatch(self, request: Request, call_next):
+#         # Call the next middleware
+#         response = await call_next(request)
         
-        # Add CORS headers to the response
-        response.headers["Access-Control-Allow-Origin"] = "*"
-        response.headers["Access-Control-Allow-Credentials"] = "true"
-        response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-        response.headers["Access-Control-Allow-Headers"] = "*"
+#         # Add CORS headers to the response
+#         response.headers["Access-Control-Allow-Origin"] = "*"
+#         response.headers["Access-Control-Allow-Credentials"] = "true"
+#         response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+#         response.headers["Access-Control-Allow-Headers"] = "*"
         
-        return response
+#         return response
 
 
 
@@ -32,12 +32,12 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
 # Register the custom CORS middleware
-app.add_middleware(CustomCORSMiddleware)
+# app.add_middleware(CustomCORSMiddleware)
 
 app.include_router(auth_router)
 app.include_router(protected_router)
